b575d5b574d92749e1ff88b47e46cb5aead03b74
Alexandre S Hostert <6692269+alehostert@users.noreply.github.com>
2024-12-19 19:51:00 +0000

    feat: install prettier and add it's scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Hello Lov, you're my World! Muito obrigado pelo apoio fantástico que você tem me dado todo esse tempo. Saiba que, se você está lendo isso aqui agora, isso é tanto mérito teu, quanto meu. Obrigado por todo o impacto positivo que você traz pra minha vida 🥰</h1>
+  return (
+    <h1>
+      Hello Lov, you're my World! Muito obrigado pelo apoio fantástico que você
+      tem me dado todo esse tempo. Saiba que, se você está lendo isso aqui
+      agora, isso é tanto mérito teu, quanto meu. Obrigado por todo o impacto
+      positivo que você traz pra minha vida 🥰
+    </h1>
+  );
 }
 
 export default Home;
